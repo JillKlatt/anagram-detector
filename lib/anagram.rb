4f1c2ab52fc21cd673480a192108699dee4a6b77
JillKlatt <klatt.jill@gmail.com>
2021-02-09 16:00:10 -0600

    commit tree 1e8e23467759f77040f663438def4a7b23141b89

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+attr_accessor :words
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+    word_array.select do |word|
+        word.split("").sort == @word.split("").sort
+    end
+    end
+# binding.pry
+    # if x.match?
+    #     return x
+    # else 
+    #     return []
+end
